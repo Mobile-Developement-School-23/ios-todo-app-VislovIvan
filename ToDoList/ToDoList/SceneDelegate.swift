279146f974d3aee36d1952089b35d4fa5b71361a
VislovIvan <vislov.ivan@bk.ru>
2023-06-15 22:32:32 +0300

    Start To-Do App Project

---
+++ ToDoList/ToDoList/SceneDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+    
+    func sceneDidDisconnect(_: UIScene) {}
+
+    func sceneDidBecomeActive(_: UIScene) {}
+
+    func sceneWillResignActive(_: UIScene) {}
+
+    func sceneWillEnterForeground(_: UIScene) {}
+
+    func sceneDidEnterBackground(_: UIScene) {}
+}
